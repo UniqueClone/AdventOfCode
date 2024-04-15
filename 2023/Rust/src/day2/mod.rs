35ea89284a4544faa748d1d8618205c253a23055
Ryan Lynch <lynchryan2@gmail.com>
2024-04-15 01:49:14 +0100

    Adding 2023 Day2 Part2

--- 2023/Rust/src/day2/mod.rs
+++ 2023/Rust/src/day2/mod.rs
@@ -14,7 +14,7 @@ pub fn day2(file_path: String) {
 
     match input {
         1 => part1(file_path),
-        // 2 => part2(file_path),
+        2 => part2(file_path),
         _ => println!("No such part"),
     }
 }
@@ -64,6 +64,43 @@ fn part1(file_path: String) {
     println!("{}", total);
 }
 
-// fn part2(file_path: String) {
-// let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-// }
+fn part2(file_path: String) {
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    let mut sum = 0;
+
+    for line in contents.lines() {
+        let input = line.split(" ").collect::<Vec<&str>>();
+
+        let mut red = 0;
+        let mut green = 0;
+        let mut blue = 0;
+
+        let mut num = 0;
+
+        for word in input {
+            if word.to_string().parse::<i32>().is_ok() {
+                num = word.parse::<i32>().expect("Failed to parse number");
+            } else {
+                match word.replace(",", "").replace(";", "").as_str() {
+                    "red" => {
+                        red = max(num, red);
+                        num = 0;
+                    }
+                    "green" => {
+                        green = max(num, green);
+                        num = 0;
+                    }
+                    "blue" => {
+                        blue = max(num, blue);
+                        num = 0;
+                    }
+                    _ => (),
+                }
+            }
+        }
+
+        sum += red * green * blue;
+    }
+    println!("{}", sum);
+}
