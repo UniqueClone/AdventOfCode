cc5b9077fd9c10e2b77822d161410bddc0fbc998
Ryan Lynch <lynchryan2@gmail.com>
2024-12-06 20:28:58 +0000

    Adding day 2 part 2 problem statement and early work.

--- 2024/Rust/src/day2/mod.rs
+++ 2024/Rust/src/day2/mod.rs
@@ -13,7 +13,7 @@ pub fn day2(file_path: String) {
 
     match input {
         1 => part1(file_path),
-        // 2 => part2(file_path),
+        2 => part2(file_path),
         _ => println!("No such part"),
     }
 }
@@ -21,10 +21,9 @@ pub fn day2(file_path: String) {
 fn part1(file_path: String) {
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let mut total_safe = 0;
-    let mut report_safe = true;
 
     for line in contents.lines() {
-        report_safe = true;
+        let mut report_safe = true;
         let mut prev: i32 = 0;
         let mut increasing: bool = false;
 
@@ -75,6 +74,94 @@ fn part1(file_path: String) {
                     report_safe = false;
                     prev = current;
                     break;
+                }
+                prev = current;
+            }
+
+            if i == line.len() - 1 {
+                println!();
+            }
+        }
+        println!("Report {} safe: {}", line, report_safe);
+        if report_safe {
+            total_safe += 1;
+        }
+    }
+    println!("{}", total_safe);
+}
+
+/**
+ * Part 2
+ * Allow a single unsafe in each report to be safe.
+ * UNFINISHED
+ */
+fn part2(file_path: String) {
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let mut total_safe = 0;
+    let mut unsafe_count = 0;
+
+    for line in contents.lines() {
+        let mut report_safe = true;
+        let mut prev: i32 = 0;
+        let mut increasing: bool = false;
+
+        // iterate over the numbers in the line
+        for (i, num) in line.split_whitespace().enumerate() {
+            // let cur_char = char.to_string();
+
+            let current = match num.parse::<i32>() {
+                Ok(n) => n,
+                Err(_) => {
+                    println!("Failed to parse number: {}", num);
+                    report_safe = false;
+                    break;
+                }
+            };
+
+            if i == 0 {
+                prev = current;
+                continue;
+            } else if current == prev {
+                println!();
+                unsafe_count += 1;
+                if unsafe_count > 1 {
+                    report_safe = false;
+                }
+                prev = current;
+            } else if i == 1 {
+                if (current - prev).abs() > 3 {
+                    println!();
+                    unsafe_count += 1;
+                    if unsafe_count > 1 {
+                        report_safe = false;
+                    }
+                    prev = current;
+                }
+                increasing = current > prev;
+                prev = current;
+                continue;
+            } else {
+                if (current - prev).abs() > 3 {
+                    println!();
+                    unsafe_count += 1;
+                    if unsafe_count > 1 {
+                        report_safe = false;
+                    }
+                    prev = current;
+                } else if increasing && current < prev {
+                    println!();
+                    unsafe_count += 1;
+                    if unsafe_count > 1 {
+                        report_safe = false;
+                    }
+                    prev = current;
+                } else if !increasing && current > prev {
+                    println!();
+                    unsafe_count += 1;
+                    if unsafe_count > 1 {
+                        report_safe = false;
+                    }
+                    prev = current;
                 }
                 prev = current;
             }
